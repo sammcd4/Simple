fad286d6175ce21d55545645dc675467bc0c0f88
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2020-01-31 22:52:17 -0500

    Initial Commit

---
+++ Simple/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Simple
+//
+//  Created by Sam McDonald on 1/31/20.
+//  Copyright © 2020 Sam McDonald. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
